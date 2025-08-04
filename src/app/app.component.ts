b20f641c3fe860e6933f39e32ab2fffcbcfedbbe
Rula-Sh <1.rulashaban@gmail.com>
2025-08-04 20:56:57 +0300

    Install Cloudinary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,32 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './pages/main/home/home.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { CloudinaryModule } from '@cloudinary/ng';
+import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen/index';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CloudinaryModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Drivora';
+
+  img!: CloudinaryImage;
+
+  ngOnInit() {
+    // Create a Cloudinary instance and set your cloud name. (cloudName value to your cloud name (found on the Cloudinary Console Dashboard)
+    const cld = new Cloudinary({
+      cloud: {
+        cloudName: 'dnoxelx9f',
+      },
+    });
+  }
 }
