a167af4946b95934e19ef3a040487397f14ec694
Rula-Sh <1.rulashaban@gmail.com>
2025-09-07 09:15:33 +0300

    Add car-details Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/customer/pages/home/home.component';
 import { CarsComponent } from './features/customer/pages/cars/cars.component';
+import { CarDetailsComponent } from './features/common/car-details/car-details.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'cars',
     component: CarsComponent,
+  },
+  {
+    path: 'car-details/:id',
+    component: CarDetailsComponent,
   },
 ];
