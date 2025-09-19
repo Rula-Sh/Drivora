7cc02b6615e783ff1ee86560ec79dd95c1313064
Rula-Sh <1.rulashaban@gmail.com>
2025-09-19 15:31:20 +0300

    Add viewCarDetails & viewCarHighlights

--- src/app/features/common/car-details/car-details.component.ts
+++ src/app/features/common/car-details/car-details.component.ts
@@ -12,6 +12,19 @@ import { ActivatedRoute } from '@angular/router';
 export class CarDetailsComponent {
   car: Car | null = null;
   carID: string = '';
+  carHighlights: {
+    key: string;
+    value: string | number;
+    name: string;
+    icon: string;
+  }[] = [];
+  carProperties: {
+    key: string;
+    value: string | number;
+    name: string;
+    icon: string;
+  }[] = [];
+
   constructor(private carService: CarService, private router: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -20,10 +33,68 @@ export class CarDetailsComponent {
       next: (car) => {
         this.car = car;
         console.log('Fetched Car Details: ', car);
+        this.viewCarHighlights();
+        this.viewCarDetails();
       },
       error: (err) => {
         console.error('Error Fetching Car Details: ', err);
       },
     });
+  }
+
+  viewCarHighlights() {
+    this.carHighlights =
+      this.car?.highlights?.map((key, value) => ({
+        key,
+        value,
+        name: this.splitCamelCase(key),
+        icon: `https://res.cloudinary.com/dnoxelx9f/image/upload/v1756714742/${key}.png`, // TODO: Update Cloudinary path
+      })) || [];
+  }
+
+  viewCarDetails() {
+    const excludedKeys: (keyof Car)[] = [
+      // exclude these keys from being displayed as properties
+      'id',
+      'licensePlate',
+      'make',
+      'model',
+      'year',
+      'color',
+      'location',
+      'images',
+      'description',
+      'features',
+      'isAvailable',
+      'saleType',
+      'price',
+      'currency',
+      'rentalPeriod',
+      'condition',
+      'registrationExpiry',
+      'insuranceExpiry',
+      'ownerId',
+      'customerId',
+    ];
+    const highlightKeys = this.carHighlights.map((h) => h.key);
+
+    this.carProperties = Object.entries(this.car!)
+      .filter(
+        ([key, value]) =>
+          value !== undefined &&
+          !excludedKeys.includes(key as keyof Car) &&
+          !highlightKeys.includes(key)
+      )
+      .map(([key, value]) => ({
+        key,
+        value,
+        name: this.splitCamelCase(key),
+        icon: `https://res.cloudinary.com/dnoxelx9f/image/upload/v1756714742/${key}.png`, // TODO: Update Cloudinary path
+      }));
+  }
+  splitCamelCase(str: string): string {
+    return str
+      .replace(/([A-Z])/g, ' $1') // insert space before uppercase letters
+      .replace(/^./, (s) => s.toUpperCase()); // capitalize first letter
   }
 }
